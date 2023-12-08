8f9e5045e8a4bcc9a55ec0996bf9b3c6d4210ea0
DAVID <david@DAVIDs-MacBook-Pro.local>
2023-12-08 12:34:08 +0200

    added firebase config to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBgFfE-7OUTRI1R_VwsANfer_4BFKXMeUA",
+    authDomain: "password-manager-bfcaf.firebaseapp.com",
+    projectId: "password-manager-bfcaf",
+    storageBucket: "password-manager-bfcaf.appspot.com",
+    messagingSenderId: "939450406732",
+    appId: "1:939450406732:web:fb190010b4c3c4958ee1e9",
+    measurementId: "G-Z37S6RW4QW"
+  }
 };
